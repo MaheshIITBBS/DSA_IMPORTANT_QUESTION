8a4d0bbe9ef01135027dfbbdd493eedb5b43fce2
MaheshIITBBS <138322804+MaheshIITBBS@users.noreply.github.com>
2023-07-01 23:44:40 +0530

    Create FIRSTCODE.py
    first python code

---
+++ MY_NOTES/FIRSTCODE.py
@@ -0,0 +1,22 @@
+""" print("mahesh is hardworking person")
+print(10*2)
+
+rent = 200
+expenses = 500
+savings = rent - expenses + 500
+
+print(savings)
+
+print(2**4)
+ """
+
+
+
+a = 10
+b = 25.245587564
+c = a+b
+d=round(c,0)
+print(d)
+
+print(10+3*2/2)
+
